396c863dec5a3b296c09a9e70db1ccd38f339730
mamrezn7 <mrz_shirazi@yahoo.com>
2024-07-01 23:19:59 +0330

    Final with database backup

--- back/server/server.js
+++ back/server/server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`im running bitch ${port}`));
+server.listen(port, () => console.log(`im running ${port}`));
